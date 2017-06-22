c394ab70dc5b2bffb4a4b3b4eea571b80678cbcb
Jack Mallahan <jackmallahan@gmail.com>
2017-06-22 12:20:48 -0600

    made canvas

---
+++ game.js
@@ -0,0 +1,30 @@
+;(function() {
+  var Game = function(canvasId) {
+     var canvas = document.getElementById(canvasId);
+     var screen = canvas.getContext('2d');
+     var gameSize = {x: canvas.width, y: canvas.height};
+
+     var self = this;
+     var tick = function() {
+      self.update();
+      self.draw(screen, gameSize);
+      requestAnimationFrame(tick);
+     };
+
+     tick();
+  };
+
+  Game.prototype = {
+    update: function() {
+      console.log('hi');
+    },
+
+    draw: function() {
+      
+    }
+  };
+
+  window.onload = function() {
+    new Game("screen");
+  }
+})();
